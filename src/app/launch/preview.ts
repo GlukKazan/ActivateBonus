b4ffa366a7ef85780c18bb66aa4fcc3f514e3961
GlukKazan <glukkazan@gmail.com>
2020-10-28 16:20:25 +0300

    Join to Session

--- src/app/launch/preview.ts
+++
@@ -1,9 +0,0 @@
-export class Preview {
-    constructor(
-      public id: number, 
-      public filename: string,
-      public selector_value: number,
-      public preview: string
-    ) {}
-  }
-  
