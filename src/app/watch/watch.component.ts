b0b104e87268326918785cc4994c118ba22f373a
GlukKazan <glukkazan@gmail.com>
2020-10-29 14:21:11 +0300

    stable

--- src/app/watch/watch.component.ts
+++ src/app/watch/watch.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Session } from '../interface/session';
 import { WatchService } from './watch.service';
 
 @Component({
@@ -10,11 +11,41 @@ import { WatchService } from './watch.service';
 })
 export class WatchComponent implements OnInit {
 
+  sessions: Array<Session>;
+  
   constructor(
     private serv: WatchService,
     private router: Router
-  ) { }
+  ) { 
+    this.sessions = new Array<Session>();
+  }
 
-  ngOnInit(): void { }
+  ngOnInit(): void { 
+    this.loadSessions();
+  }
 
+  private loadSessions() {
+    this.serv.getSessions().subscribe((data: Session[]) => {
+      this.sessions = data;
+    },
+    (error: any) => {
+      let status = error.status;
+      if ([401, 403].includes(status)) {
+        this.router.navigate(['']);
+      } else {
+        alert("Error: " + status);
+      }
+    });
+  }
+
+  public join(it: Session) {
+    if (!confirm("Join to Session?")) return;
+    let url = '/dagaz/' + it.filename + '.html?sid=' + it.id;
+    if (it.selector_value > 0) {
+      url = url + '&selector=' + it.selector_value;
+    }
+    if (url) {
+      window.location.href = url;
+    }
+  }
 }
--- src/app/watch/watch.service.ts
+++ src/app/watch/watch.service.ts
@@ -1,10 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class WatchService {
 
-  private url = '/api/session'
+  private sess = '/api/session/active'
 
   constructor(private http: HttpClient) { }
+
+  getSessions(): Observable<Object> {
+    return this.http.get(this.sess);
+  } 
 }
